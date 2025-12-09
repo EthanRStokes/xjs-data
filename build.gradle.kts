43d626c156f5b3524c48eeb5fe9c60d2e0135a8a
Ethan Stokes <erstokes10@gmail.com>
2025-12-08 18:40:25 -0600

    add junit platform launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,10 @@ repositories {
 dependencies {
     implementation("org.jetbrains:annotations:23.0.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.14.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.14.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.1")
 
     testImplementation("com.google.jimfs:jimfs:1.3.1")
     testImplementation("org.apache.commons:commons-lang3:3.18.0")
